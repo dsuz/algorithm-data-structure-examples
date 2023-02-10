b05347daa7a63811bd9e40ae439c91ea80df017e
D. Suzuki <bboydaisuke@gmail.com>
2023-02-11 00:26:26 +0900

    スタックのサンプルを追加した

---
+++ Assets/Stack/WindowController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// ウインドウを制御するコンポーネント
+/// このコンポーネントを無効にすると、ボタンを押せなくなるよう制御する
+/// コンポーネントを無効→有効にすると、最後にクリックしたボタンが選択される
+/// </summary>
+public class WindowController : MonoBehaviour
+{
+    /// <summary>ボタンのプレハブ</summary>
+    [SerializeField] Button _buttonPrefab;
+    /// <summary>ボタンを置くルートオブジェクト</summary>
+    [SerializeField] Transform _buttonRoot;
+    [SerializeField] AudioSource _audio;
+    /// <summary>操作した時に鳴る音</summary>
+    [SerializeField] AudioClip _selectSoundEffect;
+    /// <summary>最後にクリックされたボタン</summary>
+    Button _lastClickedButton;
+
+    void Start()
+    {
+        // ボタンを８個並べて、操作した時のコールバックを設定する
+        for (int i = 0; i < 8; i++)
+        {
+            var button = Instantiate(_buttonPrefab, _buttonRoot);
+            
+            if (i == 0)
+                button.Select();
+
+            button.onClick.AddListener(() =>
+            {
+                _lastClickedButton = button;
+                WindowManager.Instance.CreateNewWindow(button.transform.position);
+            });
+
+            button.OnSelectAsObservable().Subscribe(new MenuSfx(_audio, _selectSoundEffect));
+        }
+    }
+
+    void OnDisable()
+    {
+        Array.ForEach(GetComponentsInChildren<Button>(), b => b.interactable = false);
+    }
+
+    void OnEnable()
+    {
+        Array.ForEach(GetComponentsInChildren<Button>(), b => b.interactable = true);
+
+        if (_lastClickedButton)
+            _lastClickedButton.Select();
+    }
+}
+
+class MenuSfx : IObserver<BaseEventData>
+{
+    AudioSource _audioSource;
+    AudioClip _audioClip;
+
+    public MenuSfx(AudioSource audioSource, AudioClip audioClip)
+    {
+        _audioSource = audioSource;
+        _audioClip = audioClip;
+        _audioSource.clip = _audioClip;
+    }
+
+    public void OnNext(BaseEventData value)
+    {
+        _audioSource.Play();
+    }
+
+    public void OnCompleted() {}
+
+    public void OnError(Exception error)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Stack/WindowManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スタックでウインドウを管理するコンポーネント
+/// シングルトンパターンである
+/// </summary>
+public class WindowManager : MonoBehaviour
+{
+    /// <summary>ウインドウのプレハブ</summary>
+    [SerializeField] WindowController _windowPrefab;
+    /// <summary>ウインドウのルートとなるオブジェクト</summary>
+    [SerializeField] Transform _windowRoot;
+    /// <summary>表示されているウインドウを入れておくスタック</summary>
+    Stack<WindowController> _windowStack = new Stack<WindowController>();
+
+    void Start()
+    {
+        CreateNewWindow(_windowRoot.position);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            CloseActiveWindow();
+    }
+
+    /// <summary>
+    /// 新しいウインドウを開く
+    /// </summary>
+    /// <param name="position">ウインドウを生成する座標</param>
+    public void CreateNewWindow(Vector2 position)
+    {
+        WindowController window = default;
+
+        if (_windowStack.Count > 0)
+        {
+            // アクティブウインドウを無効にする
+            window = _windowStack.Pop();
+            window.enabled = false;
+            _windowStack.Push(window);
+        }
+
+        // 新しいウインドウを作る
+        window = Instantiate(_windowPrefab, _windowRoot);
+        window.transform.SetAsLastSibling();
+        window.transform.position = position;
+        _windowStack.Push(window);
+    }
+
+    /// <summary>
+    /// アクティブウインドウを開き、別のウインドウをアクティブにする
+    /// </summary>
+    public void CloseActiveWindow()
+    {
+        if (_windowStack.Count > 0)
+        {
+            Destroy(_windowStack.Pop().gameObject);
+        }
+
+        if (_windowStack.Count > 0)
+        {
+            var window = _windowStack.Pop();
+            window.enabled = true;
+            _windowStack.Push(window);
+        }
+    }
+
+    #region シングルトンパターンのためのコード
+    /// <summary>シングルトンのインスタンスを保存しておく static 変数</summary>
+    static WindowManager instance;
+
+    public static WindowManager Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                SetupInstance();
+            }
+
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    static void SetupInstance()
+    {
+        instance = FindObjectOfType<WindowManager>();
+
+        if (!instance)
+        {
+            GameObject go = new GameObject();
+            instance = go.AddComponent<WindowManager>();
+            go.name = instance.GetType().Name;
+            DontDestroyOnLoad(go);
+        }
+    }
+    #endregion
+}
