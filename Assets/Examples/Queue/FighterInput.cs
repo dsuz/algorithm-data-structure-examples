2b4be27ce461ae95daca6fce625534a8686bb8d5
D. Suzuki <bboydaisuke@gmail.com>
2023-02-12 05:57:01 +0900

    入力をキューイングして表示する処理を作成中

---
+++ Assets/Examples/Queue/FighterInput.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// コメント
+public class FighterInput : MonoBehaviour
+{
+    [Tooltip("コマンド入力の有効期間（秒）")]
+    [SerializeField] float _timeInQueue = 0.3f;
+    [Tooltip("入力キューをクリーンアップする間隔（秒）")]
+    [SerializeField] float _cleanupQueueInterval = 0.1f;
+    Queue<InputData> _inputQueue = new Queue<InputData>();
+    Coroutine _cleanupQueue;
+
+    public Queue<InputData> InputQueue => _inputQueue;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed) 
+        {
+            InputData inputData = new InputData(context.ReadValue<Vector2>(), Time.timeSinceLevelLoad);
+            _inputQueue.Enqueue(inputData);
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("Attack");
+        }
+    }
+
+    /// <summary>
+    /// 有効期限の切れた入力をキューから削除する
+    /// </summary>
+    IEnumerator CleanUpInputQueue()
+    {
+        while (true)
+        {
+            while (_inputQueue.Count > 0)
+            {
+                if (_inputQueue.Peek().Time + _timeInQueue < Time.timeSinceLevelLoad)
+                {
+                    _inputQueue.Dequeue();
+                }
+                else
+                    break;
+            }
+
+            yield return new WaitForSeconds(_cleanupQueueInterval);
+        }
+    }
+
+    void OnEnable()
+    {
+        _cleanupQueue = StartCoroutine(CleanUpInputQueue());
+    }
+
+    void OnDisable()
+    {
+        StopCoroutine(_cleanupQueue);
+    }
+}
+
+public class InputData
+{
+    Vector2 _direction;
+    float _time;
+
+    public Vector2 Direction => _direction;
+    public float Time => _time;
+
+    InputData() { }
+
+    public InputData(Vector2 direction, float time)
+    {
+        _direction = direction;
+        _time = time;
+    }
+
+    public override string ToString()
+    {
+        return $"{_direction.ToString()}, {_time.ToString("0.000")}";
+    }
+}
---
+++ Assets/Examples/Queue/InputQueueDisplay.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+/// <summary>
+/// コメント
+/// </summary>
+public class InputQueueDisplay : MonoBehaviour
+{
+    [SerializeField] FighterInput _input;
+    [SerializeField] Sprite _upArrow;
+    [SerializeField] Sprite _downArrow;
+    [SerializeField] Sprite _rightArrow;
+    [SerializeField] Sprite _leftArrow;
+    [SerializeField] GridLayoutGroup _displayRoot;
+    [SerializeField] Color _arrowColor = Color.red;
+
+    void Start()
+    {
+        _input.InputQueue.ObserveEveryValueChanged(queue => queue.Count)
+            .Subscribe(_ => UpdateQueueDisplay(_input.InputQueue.ToArray()));
+    }
+
+    void UpdateQueueDisplay(InputData[] inputArray)
+    {
+        foreach (Transform tx in _displayRoot.transform)
+        {
+            Destroy(tx.gameObject);
+        }
+
+        foreach (InputData input in inputArray)
+        {
+            Sprite sprite = null;
+
+            if (input.Direction.normalized == Vector2.up)
+                sprite = _upArrow;
+            else if (input.Direction.normalized == Vector2.down)
+                sprite = _downArrow;
+            else if (input.Direction.normalized == Vector2.right)
+                sprite = _rightArrow;
+            else if (input.Direction.normalized == Vector2.left)
+                sprite = _leftArrow;
+            else
+                continue;
+
+            GameObject arrow = new GameObject(sprite.name, typeof(RectTransform), typeof(Image));
+            var arrowImage = arrow.GetComponent<Image>();
+            arrowImage.sprite = sprite;
+            arrowImage.color = _arrowColor;
+            arrow.transform.SetParent(_displayRoot.transform);
+            arrow.transform.localScale = Vector3.one;
+        }
+    }
+}
